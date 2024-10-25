916a47b35c2a3c6cf99a554d9ee588365e6a588d
drawcodeboy <20rnjsekdns@naver.com>
2024-10-25 20:10:42 +0900

    [Fix] Attention Embedding dimension

--- models/TestNet/testnet.py
+++ models/TestNet/testnet.py
@@ -5,10 +5,11 @@ class TestNet(nn.Module):
     def __init__(self):
         super().__init__()
         
-        self.attn = nn.MultiheadAttention(embed_dim=5000, num_heads=1, batch_first=True)
+        self.attn = nn.MultiheadAttention(embed_dim=1, num_heads=1, batch_first=True)
         self.relu = nn.ReLU()
         self.li = nn.Linear(5000, 7)
     
     def forward(self, x1, x2):
         x = x1 + x2
-        return self.li(self.relu(self.attn(x, x, x)[0]))
+        x = self.attn(x, x, x)[0].reshape(x.shape[0], -1)
+        return self.li(self.relu(x))
