b3eb4b378b6c653ea49cd816688161513c3a3a19
drawcodeboy <20rnjsekdns@naver.com>
2024-10-25 19:17:15 +0900

    [Add] utils(engine, save_ckpt, metrics)

---
+++ utils/engine.py
@@ -0,0 +1,64 @@
+from .metrics import get_metrics
+
+import torch
+import torch.nn.functional as F
+import numpy as np
+from typing import Dict
+import sys
+
+def train_one_epoch(model, dataloader, optimizer, loss_fn, scheduler, device):
+    model.train()
+    
+    total_loss = []
+    for batch_idx, (x_ecg, x_pcg, targets) in enumerate(dataloader, start=1):
+        optimizer.zero_grad()
+        
+        x_ecg = x_ecg.to(device)
+        x_pcg = x_pcg.to(device)
+        
+        targets = targets.reshape(-1)
+        
+        logits = model(x_ecg, x_pcg)
+        
+        loss = loss_fn(logits, targets)
+        total_loss.append(loss.item())
+        
+        loss.backward()
+        optimizer.step()
+        
+        print(f"\rTraining: {100*batch_idx/len(dataloader):.2f}%, Loss: {sum(total_loss)/len(total_loss):.6f}, LR: {scheduler.get_last_lr()[0]:.8f}", end="")
+    print()
+    
+    scheduler.step(sum(total_loss)/len(total_loss))
+    
+    return sum(total_loss)/len(total_loss) # One Epoch Loss
+
+@torch.no_grad()
+def evaluate(model, dataloader, device) -> Dict[str, float]:
+    model.eval()
+    
+    total_outputs = []
+    total_targets = []
+    
+    for batch_idx, (x_ecg, x_pcg, targets) in enumerate(dataloader, start=1):
+        x_ecg = x_ecg.to(device)
+        x_pcg = x_pcg.to(device)
+        
+        targets = targets.reshape(-1)
+        
+        logits = model(x_ecg, x_pcg)
+        outputs = torch.argmax(F.softmax(logits, dim=1), dim=1)
+        
+        total_outputs.extend(outputs.tolist())
+        total_targets.extend(targets.tolist())
+        
+        print(f"\rEvaluate: {100*batch_idx/len(dataloader):.2f}%", end="")
+    print()
+    
+    result = get_metrics(np.array(total_outputs), np.array(total_targets))
+    
+    return result
+    
+def inference_time():
+    # Time 측정하는 함수
+    pass
---
+++ utils/metrics.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+def get_metrics(outputs, targets, class_num:int=7):
+    results = {
+        "Accuracy": 0.,
+        "F1-Score(Micro)":0.,
+        "Precision(Micro)":0.,
+        "Recall(Micro)":0.,
+        "F1-Score(Macro)":0.,
+        "Precision(Macro)":0.,
+        "Recall(Macro)":0.,
+    }
+    
+    result_matrix = np.zeros((class_num, 4)) # [cls0: [TP, FP, TN, FN], cls1: [TP, FP, TN, FN], ...]
+    
+    for cls in range(class_num):
+        # cls를 Positive 취급
+        binary_targets = (targets == cls).astype(int)
+        binary_outputs = (outputs == cls).astype(int)
+        
+        tn, fp, fn, tp = confusion_matrix(binary_targets, binary_outputs, labels=[0, 1]).ravel()
+        
+        result_matrix[cls] = np.array([tp, fp, tn, fn])
+    
+    micro_infos = result_matrix.sum(axis=0)
+    
+    results['Accuracy'] = (micro_infos[0]+micro_infos[2])/micro_infos.sum()
+    results['Precision(Micro)'] = (micro_infos[0])/(micro_infos[0]+micro_infos[1])
+    results['Recall(Micro)'] = (micro_infos[0])/(micro_infos[0]+micro_infos[3])
+    results['F1-Score(Micro)'] = (2*results['Precision(Micro)']*results['Recall(Micro)'])/(results['Precision(Micro)']+results['Recall(Micro)'])
+    
+    f1_macro, pre_macro, rec_macro = [], [], []
+    for cls_res in result_matrix:
+        precision_temp = (cls_res[0])/(1e-7 if (cls_res[0]+cls_res[1]) == 0 else (cls_res[0]+cls_res[1]))
+        recall_temp = (cls_res[0])/(1e-7 if (cls_res[0]+cls_res[3]) == 0 else (cls_res[0]+cls_res[3]))
+        pre_macro.append(precision_temp)
+        rec_macro.append(recall_temp)
+        f1_temp = (2*precision_temp*recall_temp)/(1e-7 if (precision_temp+recall_temp) == 0 else (precision_temp+recall_temp)) 
+        f1_macro.append(f1_temp)
+        
+    results['Precision(Macro)'] = sum(pre_macro)/class_num
+    results['Recall(Macro)'] = sum(rec_macro)/class_num                     
+    results['F1-Score(Macro)'] = sum(f1_macro)/class_num
+        
+    return results
