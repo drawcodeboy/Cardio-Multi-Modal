71a00e3f96c8e146690f9a40c81374900adb1b1b
drawcodeboy <20rnjsekdns@naver.com>
2024-10-25 13:33:58 +0900

    [Update] reduce data load time & downsampling

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -1,6 +1,7 @@
 from .ephnogram_dataloader import ephnogram_dataloader
 
-def load_dataset(dataset:str="EPHNOGRAM"):
+def load_dataset(dataset:str="EPHNOGRAM",
+                 mode:str='train'):
     
     if dataset == "EPHNOGRAM":
-        return ephnogram_dataloader()
+        return ephnogram_dataloader(mode=mode)
--- dataloader/ephnogram_dataloader.py
+++ dataloader/ephnogram_dataloader.py
@@ -4,13 +4,17 @@ from torch.utils.data import Dataset
 import os
 import wfdb
 import pandas as pd
+import random
+from scipy.signal import decimate
 
 class ephnogram_dataloader(Dataset):
     def __init__(self,
                  dataset_path=r"data/physionet.org/files/ephnogram/1.0.0",
                  mode='train',
+                 split_ratio:float=0.7,
                  seq_seconds:int=5, #seconds
-                 transform=None):
+                 transform=None,
+                 check_load:bool=False):
         super().__init__()
         
         # (1) Initialization
@@ -21,6 +25,10 @@ class ephnogram_dataloader(Dataset):
         if mode not in ['train', 'val', 'test']:
             raise ValueError(f"Check Dataset [mode] arg, your [mode] is {mode}")
         self.mode = mode
+        
+        self.split_ratio = split_ratio
+        
+        self.check_load = check_load
         
         self.seq_seconds = seq_seconds
         self.fs = 8000
@@ -42,6 +50,16 @@ class ephnogram_dataloader(Dataset):
         self.data_li = [] # element is list that contains [sample_path, start_point, label]
         self._check()
         
+        # (3) Randomly Sample, fixed seed = 42
+        random.seed(42)
+        random.shuffle(self.data_li)
+        
+        # (4) mode
+        if self.mode == 'train':
+            self.data_li = self.data_li[:int(self.split_ratio*len(self.data_li))]
+        elif self.mode == 'test':
+            self.data_li = self.data_li[int(self.split_ratio*len(self.data_li)):]
+        
         self.transform = transform
         
     def __len__(self):
@@ -50,15 +68,20 @@ class ephnogram_dataloader(Dataset):
     def __getitem__(self, idx):
         sample_path, start_point, label = self.data_li[idx]
         
-        sample = wfdb.rdsamp(sample_path)[0][start_point:start_point+self.sample_len]
+        # sample = wfdb.rdsamp(sample_path)[0][start_point:start_point+self.sample_len]
+        # 위 코드는 느림.
+        sample = wfdb.rdsamp(sample_path, 
+                             sampfrom=start_point, 
+                             sampto=start_point+self.sample_len)[0]
         x_ecg, x_pcg = sample[:, 0], sample[:, 1]
         
         if start_point == 0: # Issue: PCG는 start point가 항상 1임 -> t_0 -> 0으로 초기화
             x_pcg[0] = 0.
             
-        # Implementation List (transform)
-        # (1) Downsampling
-        # (2) transform for preprocessing (Normalization)
+        # Downsampling (8000Hz -> 1000Hz)
+        x_ecg = decimate(x_ecg, 8)
+        x_pcg = decimate(x_pcg, 8)
+        
         if self.transform is not None:
             x_ecg, x_pcg = self.transform(x_ecg, x_pcg)
             
@@ -71,7 +94,7 @@ class ephnogram_dataloader(Dataset):
         metadata = pd.read_csv(f"{self.dataset_path}/ECGPCGSpreadsheet.csv")
         
         for i in range(metadata.shape[0]):
-            # if i == 2: break # for debugging
+            # if i == 3: break # for debugging
             print(f"\rLoad Dataset[{i+1:02d}/{metadata.shape[0]:02d}] ({100*(i+1)/metadata.shape[0]:.2f}%)", end="")
             
             row = metadata.iloc[i]
@@ -82,16 +105,21 @@ class ephnogram_dataloader(Dataset):
             
             # Read Sample
             sample_path = f"{self.dataset_path}/WFDB/{row['Record Name']}"
-            try:
-                sample = wfdb.rdsamp(sample_path)
-            except:
-                continue
+            
+            if self.check_load == True:
+                try:
+                    sample = wfdb.rdsamp(sample_path)
+                except:
+                    continue
             
             # Set Label
             label = self.class_map[row['Recording Scenario']]
             
+            # Set Sample Total Length
+            sample_total_length = 30*8000 if row['Record Duration (min)'] == 0.5 else 30*60*8000
+            
             # Add to self.data_li
-            for start_point in range(0, sample[1]['sig_len'], self.sample_len):
+            for start_point in range(0, sample_total_length, self.sample_len):
                 self.data_li.append([sample_path, start_point, label])
             
             # label_cnt[label] += (sample[0].shape[0] // self.sample_len) # for debugging
@@ -100,12 +128,16 @@ class ephnogram_dataloader(Dataset):
         print()
 
 if __name__ == '__main__':
-    ds = ephnogram_dataloader()
+    train_ds = ephnogram_dataloader(mode='train')
+    test_ds = ephnogram_dataloader(mode='test')
     
-    x_ecg, x_pcg, label = ds[362]
+    for i, (x1, x2, label) in enumerate(train_ds):
+        print(f"\rload {i}", end='')
+    
+    x_ecg, x_pcg, label = train_ds[0]
     
     print(x_ecg.shape, x_pcg.shape)
-    print(len(ds)) # if 5secs sampling, number of samples is 22,008
+    print(len(train_ds)), print(len(test_ds)) # if 5secs sampling, number of samples is 22,008
     
     import matplotlib.pyplot as plt
     import numpy as np
