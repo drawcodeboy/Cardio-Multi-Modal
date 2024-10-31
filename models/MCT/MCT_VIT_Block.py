c0fe22f99a4ee1642e1330448e0e41ecde83717e
daecode <eotjq5802@naver.com>
2024-10-31 16:52:31 +0900

    [Add] MCT_VIT_Block

--- models/MCT/MCT_VIT_Block.py
+++ models/MCT/MCT_VIT_Block.py
@@ -2,79 +2,73 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-
-class MCT_ViT_Block(nn.Module):
-    def __init__(self, token_dim = 256, num_heads=8): # 멀티헤드 8개로 나눔
-        super(MCT_ViT_Block, self).__init__()
-        # Linear layers to match dimensions for concatenation
-        self.cls_linear = nn.Linear(token_dim, token_dim)
-        self.token_linear = nn.Linear(token_dim, token_dim)
+class MCT_VIT_Block(nn.Module):
+    def __init__(self, token_dim = 32, num_heads=8): # 멀티헤드 8개로 나눔
+        super(MCT_VIT_Block, self).__init__()
+        # 1. Linear(차원을 맞춰주기 위해)
+        self.cls_linear = nn.Linear(token_dim , token_dim//2)
+        self.token_linear = nn.Linear(token_dim, token_dim//2)
         
-        # Multiscale Conv
+        # 2. Multiscale Conv
         self.conv3 = nn.Conv1d(token_dim, token_dim, kernel_size=3, padding=1) # 동일한 출력을 위해 padding 조절
         self.conv5 = nn.Conv1d(token_dim, token_dim, kernel_size=5, padding=2)
         self.conv7 = nn.Conv1d(token_dim, token_dim, kernel_size=7, padding=3)
 
-        # Feed-forward(1)
-        self.norm1 = nn.LayerNorm(token_dim)
-        self.conv1x1 = nn.Conv1d(token_dim * 3, token_dim, kernel_size=1)  # 차원을 맞추기 위한 1x1 컨볼루션
+        # 3. Feed-forward(1)
+        self.norm_conv = nn.LayerNorm(token_dim)
+        self.conv1x1 = nn.Conv1d(token_dim, token_dim, kernel_size=1)
+        self.norm_ffn = nn.LayerNorm(token_dim)
         self.gelu = nn.GELU()
 
-        # Feed-forward(2)-(MHSA)
-        self.norm2 = nn.LayerNorm(token_dim)
+        # 4. Feed-forward(2)-(MHSA)
+        self.norm_mhsa = nn.LayerNorm(token_dim)
         self.mhsa = nn.MultiheadAttention(embed_dim = token_dim, num_heads = 8, batch_first = True)
 
-    def forward(self, cls_token, ecg_tokens, pcg_tokens):
+    def forward(self, input_tokens, cls_token):
         '''
         input
-        cls_token_shape: torch.Size([128, 256, 1])
-        ecg_tokens_shape: torch.Size([128, 313, 256]), pcg_tokens_shape: torch.Size([128, 313, 256])
+        cls_token_shape: (128, 32, 1) = (batch_size, token_dim, sequence_length)
+        ecg_tokens_shape: torch.Size([128, 32, 313]), pcg_tokens_shape: torch.Size([128, 32, 313]) = (batch_size, token_dim, sequence_length)
         '''
-        cls_token = cls_token.transpose(1, 2)  # (batch_size, 1, token_dim) # 차원 맞춰주기
+        # 1. Linear
+        cls_token = cls_token.permute(0, 2, 1)  # (batch_size, 1, token_dim) 
+        input_tokens = input_tokens.permute(0, 2, 1)  # (batch_size, sequence_length, token_dim) 
 
-        # Linear convolution 
-        cls_token_proj = self.cls_linear(cls_token)  # (batch_size, 1, token_dim)
-        ecg_tokens_proj = self.token_linear(ecg_tokens)  # (batch_size, L, token_dim)
-        pcg_tokens_proj = self.token_linear(pcg_tokens)  # (batch_size, L, token_dim)
+        cls_token_proj = self.cls_linear(cls_token)  # (batch_size, 1, token_dim/2)
+        input_tokens_proj = self.token_linear(input_tokens)  # (batch_size, sequence_length, token_dim/2)
 
         # concat(Cls token 벡터를 시퀀스의 길이만큼 repeat시킴)
-        ecg_concat = torch.cat([cls_token_proj.expand(-1, ecg_tokens_proj.size(1), -1), ecg_tokens_proj], dim=-1)  # (batch_size, L, token_dim * 2)
-        pcg_concat = torch.cat([cls_token_proj.expand(-1, pcg_tokens_proj.size(1), -1), pcg_tokens_proj], dim=-1)  # (batch_size, L, token_dim * 2)
+        token_concat = torch.cat([cls_token_proj.expand(-1, input_tokens_proj.size(1), -1), input_tokens_proj], dim=-1)  # (batch_size, sequence_length, token_dim)
         
-        # Multiscale Convolution
-        # conv를 위해 차원 변경
-        ecg_concat = ecg_concat.transpose(1, 2)  # (batch_size, token_dim * 2, L)
-        pcg_concat = pcg_concat.transpose(1, 2)  # (batch_size, token_dim * 2, L)
+        # 2. Multiscale Convolution
+        token_concat = token_concat.transpose(1, 2)  # (batch_size, token_dim, sequence_length)  
 
-        # 다중 크기의 컨볼루션 적용
-        ecg_out3 = self.conv3(ecg_concat)
-        ecg_out5 = self.conv5(ecg_concat)
-        ecg_out7 = self.conv7(ecg_concat)
-        
-        pcg_out3 = self.conv3(pcg_concat)
-        pcg_out5 = self.conv5(pcg_concat)
-        pcg_out7 = self.conv7(pcg_concat)
+        token_out3 = self.conv3(token_concat)   # (batch_size, token_dim, sequence_length)  
+        token_out5 = self.conv5(token_concat)
+        token_out7 = self.conv7(token_concat)
 
-        # 세 개의 컨볼루션 결과를 Concat
-        ecg_multiscale = torch.cat([ecg_out3, ecg_out5, ecg_out7], dim=1)  # (batch_size, token_dim * 3, L)
-        pcg_multiscale = torch.cat([pcg_out3, pcg_out5, pcg_out7], dim=1)  # (batch_size, token_dim * 3, L)
-        
-        ecg_multiscale = self.conv1x1(ecg_multiscale).transpose(1, 2)  # (batch_size, L, token_dim)
-        pcg_multiscale = self.conv1x1(pcg_multiscale).transpose(1, 2)  # (batch_size, L, token_dim)
+        token_multiscale = token_concat + token_out3 + token_out5 + token_out7 # (batch_size, token_dim, sequence_length) 
 
-        # LayerNorm
-        ecg_multiscale = self.norm1(ecg_multiscale)
-        pcg_multiscale = self.norm1(pcg_multiscale)
+        # 3. Feed-forward(1)
+        token_multiscale = token_multiscale.permute(0, 2, 1)  
+        norm_multiscale = self.norm_conv(token_multiscale) # (batch_size, sequence_length, token_dim) 
 
-        # GELU 
-        ecg_features = self.gelu(ecg_multiscale)
-        pcg_features = self.gelu(pcg_multiscale)
+        conv_input = norm_multiscale.permute(0, 2, 1)  
+        conv_output = self.conv1x1(conv_input)  # (batch_size, token_dim, sequence_length)
+        conv_output = conv_output.permute(0, 2, 1) 
 
-        # Residual Connection
-        # 여기서 부터 작성
-        
-        return ecg_output, pcg_output
+        norm_ffn = self.norm_ffn(conv_output) # (batch_size, sequence_length, token_dim) 
+        activated_ffn = self.gelu(norm_ffn)  # (batch_size, sequence_length, token_dim)
+        token_residual = token_multiscale + activated_ffn # (batch_size, sequence_length, token_dim)
+
+        # 4. Feed-forward(2)-(MHSA)
+        norm_mhsa_input = self.norm_mhsa(token_residual) # (batch_size, sequence_length, token_dim)
+        attn_output, _ = self.mhsa(norm_mhsa_input, norm_mhsa_input, norm_mhsa_input)  # (batch_size, sequence_length, token_dim)
+
+        final_output = attn_output.permute(0, 2, 1)  # (batch_size, 1, token_dim) 
+
+        # output: (batch_size, token_dim, sequence_length) 
+        return final_output
 
 
 
--- models/MCT/mct.py
+++ models/MCT/mct.py
@@ -3,43 +3,58 @@ from torch import nn
 
 from .Resnet1d_wang import ResNet1d_Wang, BasicBlock1d
 from .generate_cls_token import generate_cls_token
-from .MCT_VIT_Block import MCT_ViT_Block
+from .MCT_VIT_Block import MCT_VIT_Block
 
 class MCT_Model(nn.Module):
     def __init__(self):
         super(MCT_Model, self).__init__()
-        ## 여기에 ResNet1d, VIT 등등 모델 다 작성하기
+        # 1. Resnet1d_wang 
+        self.ecg_feature_extractor = ResNet1d_Wang(BasicBlock1d, layers=[1, 1, 1], input_channels=1, token_dim=32) # input: (batch_size, input_channels, input_length)
+        self.pcg_feature_extractor = ResNet1d_Wang(BasicBlock1d, layers=[1, 1, 1], input_channels=1, token_dim=32) # output: (batch_size, token_dim, output_length)
 
-        self.ecg_feature_extractor = ResNet1d_Wang(BasicBlock1d, layers=[1, 1, 1], input_channels=1, token_dim=256) # input: (input_channels, sequence_length)
-        self.pcg_feature_extractor = ResNet1d_Wang(BasicBlock1d, layers=[1, 1, 1], input_channels=1, token_dim=256)
-        self.linear = nn.Linear(256, 256)
-        # self.viT_block = MCT_ViT_Block()
+        # 2. cls token creating
+        self.generate_cls_token = generate_cls_token # input: ecg and pcg (batch_size, token_dim, sequence_length) * 2 # output: (batch_size, token_dim, 1)
+
+        # 3. MCT_VIT_Block
+        self.ecg_vit_block = MCT_VIT_Block() # input: ecg(batch_size, token_dim, sequence_length) and cls(batch_size, token_dim, 1)
+        self.pcg_vit_block = MCT_VIT_Block() # output: (batch_size, token_dim, sequence_length) 
+
+        # 4. FusionBlock
         # self.fusion_block = FusionBlock()
+        
+        # 5. Classifier
         # self.classifier = nn.Linear(in,ou 작성)  
 
-    def forward(self, ecg, pcg): ## pcg, ecg (batch_size, 5000, 1) 
-        # print(f'ecg_shape: {ecg.shape}, pcg_shape: {ecg.shape}')
+    def forward(self, ecg, pcg): 
+        ## pcg, ecg (batch_size, 5000, 1) 
 
-        ecg = ecg.permute(0, 2, 1)  # 차원 변경
-        pcg = pcg.permute(0, 2, 1) 
+        # 차원 변경
+        ecg = ecg.permute(0, 2, 1)  # (batch_size, 1, 5000) 
+        pcg = pcg.permute(0, 2, 1)
 
-        ecg_tokens = self.ecg_feature_extractor(ecg) # (batch_size, 1, 5000) -> (batch_size, 256, 313) -> batch, token_dim, sequence_length
-        pcg_tokens = self.pcg_feature_extractor(pcg) # (batch_size, 1, 5000) -> (batch_size, 256, 313)
-        # print(f'ecg_tokens_shape: {ecg_tokens.shape}, pcg_tokens_shape: {pcg_tokens.shape}')
+        # ResNet1d_wang 으로 토큰 추출
+        ecg_tokens = self.ecg_feature_extractor(ecg) # (batch_size, 32, 313) (batch, token_dim, sequence_length)
+        pcg_tokens = self.pcg_feature_extractor(pcg) 
 
-        cls_token = generate_cls_token(ecg_tokens, pcg_tokens)
+        # cls_token 생성
+        cls_token = self.generate_cls_token(ecg_tokens, pcg_tokens) # (batch_size, 32, 1) (batch_size, token_dim, 1)
 
-        ## MCT_VIT 작성하기
-        # cls_token_shape: torch.Size([128, 256, 1])
-        # ecg_tokens_shape: torch.Size([128, 313, 256]), pcg_tokens_shape: torch.Size([128, 313, 256])
-        ecg_feature_output1, pcg_feature_output1, cls_token_output1 = MCT_ViT_Block(ecg_tokens, pcg_tokens, cls_token)
+        # MCT_VIT 
+        '''
+        # input
+        cls_token_shape: torch.Size([128, 32, 1]) 
+        ecg_tokens_shape: torch.Size([128, 32, 313]), pcg_tokens_shape: torch.Size([128, 32, 313]) = (batch_size, token_dim, sequence_length)      
+        '''
+        ecg_feature_output = self.ecg_vit_block(ecg_tokens, cls_token) # (batch_size, token_dim, sequence_length)
+        pcg_feature_output = self.pcg_vit_block(pcg_tokens, cls_token) 
 
 
+        # FusionBlock 들어가는 것
+        # cls, ecg_feature_output, pcg_feature_output 퓨전
 
 
         while(1):
             a = 1
-
         
         
         
